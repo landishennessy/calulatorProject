cfdf00d809bb1100d7929cf7d815b6e53a2f5ad0
lando <91922837+landishennessy@users.noreply.github.com>
2022-03-19 15:19:59 -0600

    initial functions

---
+++ script.js
@@ -0,0 +1,12 @@
+const add=function(num1,num2){
+    return num1 + num2
+}
+const subtract=function(num1,num2){
+    return num1 - num2
+}
+const multiply=function(num1,num2){
+    return num2 * num1
+}
+const divide=function(num1,num2){
+    return num1/num2
+}
