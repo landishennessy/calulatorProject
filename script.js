a3db02617691111a6b4e710f2a262fa43393d967
lando <91922837+landishennessy@users.noreply.github.com>
2022-03-22 12:59:25 -0600

    all basic functionality working

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ const multiply=function(num1,num2){
     return num2 * num1
 }
 const divide=function(num1,num2){
+    if(num2 === 0){
+        document.querySelector('.calculatorScreen').textContent = 'error';
+        return "error"
+    }
     return num1/num2
 }
 
@@ -41,6 +45,8 @@ let operatorValue = '';
 let currentValue = '';
 let val1 = '';
 let val2 = '';
+let vals = [];
+
 
 
 const updateCurrent = function(num1){
@@ -49,25 +55,58 @@ const updateCurrent = function(num1){
 
 const buttons = document.querySelectorAll('.button');
 buttons.forEach(button =>{
-    button.addEventListener('click', function handleClick(event){
-       
-        document.querySelector('.calculatorScreen').textContent = button.textContent
+    button.addEventListener('click', function handleClick(event){       
+        displayValue += button.textContent;
+        console.log('display',displayValue)
+        document.querySelector('.calculatorScreen').textContent = displayValue;
+        vals.push(parseInt(button.textContent));
+
+      
        // currentValue = button.textContent
-        if(val1 === ''){
-            val1 = parseInt(button.textContent);
-           // console.log('empty')
-        }
-        else{
-            val2 = parseInt(button.textContent);
-           // console.log(button.textContent)
-        }
-       console.log('currentVal=',currentValue,'val1= ',val1, 'val2 =',val2);
+        // if(val1 === ''){
+        //     val1 = parseInt(button.textContent);
+        //     
+        //    // console.log('empty')
+        // }
+        // else{
+        //     val2 = parseInt(button.textContent);
+        //     vals.push(parseInt(button.textContent));
+        //    // console.log(button.textContent)
+        // }
+       console.log(vals);
+       val1=vals[0]
+       val2=vals[1]
+       
     });
+   
 });
+
+
+// buttons.forEach(button =>{
+//     button.addEventListener('click', function handleClick(event){       
+//         document.querySelector('.calculatorScreen').textContent = button.textContent
+//        // currentValue = button.textContent
+//         if(val1 === ''){
+//             val1 = parseInt(button.textContent);
+//             vals.push(parseInt(button.textContent));
+//            // console.log('empty')
+//         }
+//         else{
+//             val2 = parseInt(button.textContent);
+//             vals.push(parseInt(button.textContent));
+//            // console.log(button.textContent)
+//         }
+//        console.log('currentVal=',currentValue,'val1= ',val1, 'val2 =',val2);
+//     });
+// });
+
+
+
+
 const operators = document.querySelectorAll('.operator');
 operators.forEach(operator =>{
     operator.addEventListener('click', function handleClick(event){
-       
+        displayValue += operator.textContent;
         operatorValue = operator.textContent
         console.log(operatorValue);
 
@@ -77,9 +116,15 @@ operators.forEach(operator =>{
 
 
 const calculator=function(){
+       
     if(val1 != '' || val2 != '' || operatorValue != ''){
     let calculation = operate(operatorValue,val1,val2,)
     document.querySelector('.calculatorScreen').textContent = calculation
+    displayValue = calculation
+    currentValue = calculation
+    vals[0]=calculation;
+    vals.pop();
+    console.log('newarray',vals)
     console.log('calc',calculation)
    // console.log(val1,val2,operatorValue)
     }
@@ -94,4 +139,13 @@ enter.addEventListener('click', function handleClick(event){
     calculator();
 })
 calculator();
+
+const clear = document.querySelector('.calculatorClear')
+clear.addEventListener('click', function handleClick(event){
+    document.querySelector('.calculatorScreen').textContent = 'clear';
+    vals=[];
+    currentValue = 0;
+    displayValue = '';
+    console.log('clear')
+})
 
